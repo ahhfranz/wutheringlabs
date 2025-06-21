56576eff19f2b0bf678bbfcd3ec6c5ccc9194a2a
ahhfranz <ahhtryhard@gmail.com>
2025-06-20 21:53:46 -0300

    Algunos cambios

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Introduccion from '@/views/Introduccion.vue'
 import Iniciantes from '@/views/Iniciantes.vue'
@@ -53,7 +53,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
